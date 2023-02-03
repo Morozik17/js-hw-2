a5ac73f4362a697aa825d07f5b72ad01dcbc472f
Moroz Stanislav <108823770+Morozik17@users.noreply.github.com>
2023-02-03 23:13:15 +0100

    dz
    dz

---
+++ practice.js
@@ -0,0 +1,117 @@
+// Задача №1
+// Створити три змінні
+// apples = 30;
+// plums = 70;
+// Сума у змінній sum
+// і вивести результат в консоль
+
+// const apples = 30;
+// const plums = 70;
+// const result = apples + plums;
+
+// console.log(result);
+
+// Задача №1.1
+// Створити три змінні
+// bananas = 100;
+// oranges = 30;
+// Сума у змінній sum
+// і вивести результат в консоль
+
+// const bananas = 100;
+// const oranges = 30;
+// const result = bananas + oranges;
+
+// console.log(result);
+
+// Задача №1.2
+// Створити три змінні
+// strawberry = 20;
+// cherries = 15;
+// Різниця у змінній difference
+// і вивести результат в консоль
+
+
+// const strawberry = 20;
+// const cherries = 15;
+// const result = strawberry + cherries;
+
+// console.log(result);
+
+
+// ..............
+
+// Задача №2
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 10см(ПR(2)).Math.pow(a, b) - піднести до степені
+
+// const rad = 10;
+// const PI = Math.PI;
+// const powRad = Math.pow(rad, 2);
+// const result = PI * powRad;
+
+// console.log(result);
+
+// Задача №2.1
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 20см(ПR(3)).Math.pow(a, b) - піднести до степені
+
+// const rad = 20;
+// const PI = Math.PI;
+// const powRad = Math.pow(rad, 3);
+// const result = PI * powRad;
+
+// console.log(result);
+
+// Задача №2.2
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 100см(ПR(2)).Math.pow(a, b) - піднести до степені
+
+// const rad = 100;
+// const PI = Math.PI;
+// const powRad = Math.pow(rad, 2);
+// const result = PI * powRad;
+
+// console.log(result);
+
+// .................
+// Задача №3
+// Напиши скрипт який просить у користувача ввести число яке потрібно піднести до степеня,
+//і піднести це число до степерні
+//  і вивести результат в консоль
+
+// console.log(prompt("Дай число"));
+// console.log(promt("В яку степінь"));
+
+// ..................
+// Задача №4
+// Доповни код, присвоївши змінній totalPrice вираз для підрахунку загальної суми замовлення.
+// Змінна pricePerItem зберігає ціну однієї одиниці товару,
+// а orderedQuantity - кількість одиниць товару в замовленні.
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// .................
+
+// Задача №5
+// Магазин з продажу ремонтних дроїдів готовий до відкриття,
+// залишилося написати скрипт для їх замовлення.Оголоси змінні та присвой їм відповідні значення:
+
+// pricePerDroid - ціна одного дроїда, значення 800
+// orderedQuantity - кількість дроїдів у замовленні, значення 6
+// deliveryFee - вартість доставки, значення 50
+// totalPrice - загальна сума замовлення до сплати, не забудь про вартість доставки
+// результат виведи у консоль
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+
+// console.log("Ціна",totalPrice);
